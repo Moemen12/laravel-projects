75dfa385bbc32e403017d9db7e8ada2800b7181f
unknown <moemensaadeh936@gmail.com>
2023-09-09 00:38:20 +0300

    laravel-project

---
+++ job-board/resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel job Board</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        {{ $slot }}
+    </body>
+</html>
