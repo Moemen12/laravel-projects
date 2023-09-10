0e7a44336dee9d4eaaf85b3ef398a70eaf9af000
unknown <moemensaadeh936@gmail.com>
2023-09-11 02:44:07 +0300

    laravel-project

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
   @foreach ($jobs as $job)
-    <x-job class="mb-4" :$job>
+    <x-job-card class="mb-4" :$job>
       <div>
         <x-link-button :href="route('jobs.show', $job)">
           Show
         </x-link-button>
       </div>
-    </x-job>
+    </x-job-card>
   @endforeach
 </x-layout>
